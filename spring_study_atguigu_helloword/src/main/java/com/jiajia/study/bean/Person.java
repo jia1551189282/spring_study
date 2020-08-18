6cc1688ab98939ed1245d6b3afe8df8ab3dfb0e1
zjj <zjj@QQ>
2020-08-18 21:02:59 +0800

    spring hellWord 编写

---
+++ spring_study_atguigu_helloword/src/main/java/com/jiajia/study/bean/Person.java
@@ -0,0 +1,17 @@
+package com.jiajia.study.bean;
+
+import lombok.Data;
+
+/**
+ * @Author zjiajia
+ * @Date 2020/8/18 20:50
+ */
+
+@Data
+public class Person {
+
+    private String lastName;
+    private String email;
+    private String gender;
+    private Integer age;
+}
