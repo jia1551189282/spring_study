c92f96ad9151fb3030abb2045de9e7b93bdb3513
jia1551189282 <1551189282@qq.com>
2020-08-25 14:58:13 +0800

    ssm整合 -- 假如SpringMVC 框架

--- ssm/src/main/java/com/jiajia/study/controller/AccountController.java
+++ ssm/src/main/java/com/jiajia/study/controller/AccountController.java
@@ -1,6 +1,7 @@
 package com.jiajia.study.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author zjiajia
@@ -9,6 +10,13 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class AccountController {
+
+
+    @RequestMapping("/test")
+    public String controllerTest(){
+
+        return "success";
+    }
 
 
 
--- ssm/src/main/webapp/WEB-INF/pages/success.jsp
+++ ssm/src/main/webapp/WEB-INF/pages/success.jsp
@@ -2,15 +2,16 @@
   Created by IntelliJ IDEA.
   User: zjj
   Date: 2020/8/25
-  Time: 13:57
+  Time: 14:41
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>ssm整合</title>
+    <title>请求成功</title>
 </head>
 <body>
-    <a href="test">测试SpringMVC</a>
+<h1>跳转成功</h1>
+
 </body>
 </html>
